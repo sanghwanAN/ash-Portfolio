a9521c82278f5898fff58e83f4cc077f0c29d048
sanghwanAN <vooo845@gmail.com>
2021-08-22 15:28:41 +0900

    gatsby Tutorial Part 7 (튜툐리얼 완료)

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,28 @@
 import * as React from 'react'
 import { graphql } from 'gatsby' // highlight-line
 import { MDXRenderer } from 'gatsby-plugin-mdx' // highlight-line
+import { GatsbyImage, getImage } from 'gatsby-plugin-image' // highlight-line
 import Layout from '../../components/layout'
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image) // highlight-line
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      {/* highlight-start */}
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      {/* highlight-end */}
+      {/* highlight-start */}
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+      {/* highlight-end */}
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -16,13 +32,21 @@ const BlogPost = ({ data }) => {
 
 // highlight-start
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: {eq: $id}) {
+  query($slug: String) {
+    mdx(slug: {eq: $slug}) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `

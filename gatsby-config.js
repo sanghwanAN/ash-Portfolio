f95c403d407b7d5a2a71d9f8dacce3802f3a8fca
sanghwanAN <vooo845@gmail.com>
2021-08-22 14:44:56 +0900

    gatsby Tutorial Part 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "ash-portfolio",
+    title: "My First Gatsby Site",
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-sass", 
+    "gatsby-plugin-gatsby-cloud",   
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
 };
---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import { graphql } from 'gatsby' // highlight-line
+import { MDXRenderer } from 'gatsby-plugin-mdx' // highlight-line
+import Layout from '../../components/layout'
+
+const BlogPost = ({ data }) => {
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <MDXRenderer>
+        {data.mdx.body}
+      </MDXRenderer>
+    </Layout>
+  )
+}
+
+// highlight-start
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`
+// highlight-end
+
+export default BlogPost

1a4e02fb746ad0e1d4b7e4a4226c02a7caa929c2
sanghwanAN <vooo845@gmail.com>
2021-09-23 20:02:05 +0900

    about 작성중(중간커밋)

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
+    <Layout pageTitle={"Professional \n Web publisher"} pageClassName="container--about">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )

2e9c81f85fabe6f05352170d16afd174ef524282
sanghwanAN <vooo845@gmail.com>
2022-02-05 14:52:38 +0900

    getProps={isActive} 추가

--- src/components/gnb.js
+++ src/components/gnb.js
@@ -3,17 +3,19 @@ import { Link } from 'gatsby'
 
 function  Gnb () {
 
-
+  const isActive = ({ isCurrent }) => {
+    return isCurrent ? { className: "active" } : {}
+  }
   
   return (
   <ul className="header-nav__ul">
     <li className="header-nav__item" key="01">
-      <Link to="/about" className="nav-link-text" activeStyle={{ color: "tomato" }} >
+      <Link to="/about" className="nav-link-text" getProps={isActive}  activeStyle={{ color: "tomato" }} >
         About
       </Link>
     </li>
     <li className="header-nav__item"  key="02">
-      <Link to="/blog" className="nav-link-text" activeStyle={{ color: "tomato" }} >
+      <Link to="/blog" className="nav-link-text" getProps={isActive}  activeStyle={{ color: "tomato" }} >
         WorksBlog
       </Link>
     </li>
